427f0eadcb037299f86727b9840be8173f3ba872
unknown <marcelo>
2021-09-10 00:16:28 -0300

    added react-modal

--- src/components/TransacionTable/index.tsx
+++ src/components/TransacionTable/index.tsx
@@ -1,12 +1,12 @@
 import { Container } from "./styles";
 import { useEffect } from "react";
+import { api } from "../../services/api";
 
 export function TransactionTable(){
 
     useEffect(() => {
-        fetch('http://localhost:3000/api/transactions')
-            .then(response => response.json())
-            .then(data => console.log(data))
+        api.get('http://localhost:3000/api/transactions')
+            .then(response => console.log(response.data))
     }, [])
 
     return(
