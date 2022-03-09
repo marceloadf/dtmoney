1082d97a0f2a30920b314be9ef2250e636d8efa2
marcelo duarte <marceloadferreira@gmail.com>
2022-03-08 21:37:15 -0300

    added type buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,13 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
-import closeButton from '../../assets/close.svg';
+import { Container, TransactionTypeContainer } from './styles';
+import closeButtonImg from '../../assets/close.svg';
+import incomeImg from '../../assets/incomes.svg';
+import outcomeImg from '../../assets/outcomes.svg';
 
 interface NewTransactionModalProps{
     modalIsOpen: boolean;
     closeModal: () => void;
 }
-
 
 export function NewTransactionModal( {modalIsOpen, closeModal} : NewTransactionModalProps) {
     return (
@@ -22,13 +23,25 @@ export function NewTransactionModal( {modalIsOpen, closeModal} : NewTransactionM
                 onClick={closeModal} 
                 className="react-modal-close"
             >
-                <img src={closeButton} alt="Fechar Modal" />
+                <img src={closeButtonImg} alt="Fechar Modal" />
             </button>
 
            <Container>
                 <h2>Cadastrar Transaão</h2>
                 <input placeholder='Título'/>
                 <input placeholder='Valor' type='number'/>
+
+                <TransactionTypeContainer>
+                    <button type="button">
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button> 
+                    <button type="button">
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button> 
+                </TransactionTypeContainer>
+
                 <input placeholder='Categoria'/>
 
                 <button type='submit'>
--- src/components/NewTransactionModal/styles.tsx
+++ src/components/NewTransactionModal/styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.form`
 
@@ -47,6 +48,41 @@ export const Container = styled.form`
             filter: brightness(0.9);
         }
     }
-
-
 `
+
+export const TransactionTypeContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.5rem;
+    margin: 1rem 0;
+
+    button {
+        height: 4rem;
+        border: 1px solid #d7d7d7;
+
+        background: transparent;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 0.25rem;
+
+        transition: border-color 0.2s;
+
+        &:hover {
+            border-color: ${darken(0.1, "#d7d7d7")};
+        }
+
+        img {
+            height: 20px;
+            width: 20px;
+        }
+
+        span {
+            display: inline-block;
+            margin-left: 1rem;
+            font-size: 1rem;
+            color: var(--text-title);
+        }
+    }
+`;
