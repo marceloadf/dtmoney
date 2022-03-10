56ad056f8efbc27d1b207fe98e466a29dcc256db
marcelo duarte <marceloadferreira@gmail.com>
2022-03-10 20:03:12 -0300

    added transactions database to miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
     transaction: Model
   },
 
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Salário ACT Digital',
+          type: 'deposit',
+          category: 'Salário',
+          value: 9000,
+          createdAt: new Date('2021-03-15 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Despesas fixas',
+          value: 3000,
+          createdAt: new Date('2021-03-31 12:00:00'),
+        }
+      ]
+    })
+  },
+
   routes(){
     this.namespace = 'api';
    
