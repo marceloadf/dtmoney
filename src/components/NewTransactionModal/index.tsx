39e7250d1a45c655756e6c5e3df940463b71a369
marcelo duarte <marceloadferreira@gmail.com>
2022-03-10 19:33:24 -0300

    fix hot reload problem

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -25,7 +25,7 @@ export function NewTransactionModal( {modalIsOpen, closeModal} : NewTransactionM
             value,
             category
         };
-        
+        console.log(data);
         api.post("/transactions", data);
     };
 
